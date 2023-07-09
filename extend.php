b1f700452947b8ef9fccc4f3ce6cc85d6fa53c54
Mario Santagiuliana <mario@marionline.it>
2023-07-09 17:53:53 +0200

    fix

--- extend.php
+++ extend.php
@@ -16,7 +16,7 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less')
         ->content(TheRoyalsTheme::class),
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
